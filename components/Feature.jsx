1dcece18604939d25652f81d9b26e4dc617a8aec
MiguelTucto <u201521691@upc.edu.pe>
2023-10-28 21:28:07 -0500

    fix: form updating

--- components/Feature.jsx
+++ components/Feature.jsx
@@ -25,12 +25,12 @@ const Feature = () => {
             <div className={'md:px-80 md:pt-[100px] px-10 pt-5 pb-12 '}>
                 <motion.h2 initial="offscreen" whileInView="onscreen" viewport={{ once: true, amount: 0.1}} variants={cardVariants} className={'font-bold md:text-8xl text-5xl'}>Nos representa como grupo</motion.h2>
                 <div className={'flex md:flex-row flex-col items-center justify-between  mt-9'}>
-                    <div className={'flex flex-col gap-10 items-center'}>
+                    <div className={'flex flex-col gap-10 items-center h-full'}>
                         <motion.div initial="offscreen" whileInView="onscreen" viewport={{ once: true, amount: 0.1}} variants={cardVariants} className={'flex flex-row items-center gap-4'}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16 text-blue-600">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
-                            <div>
+                            <div className={'w-full'}>
                                 <h3 className={'font-bold text-xl'}>Profesionalismo</h3>
                                 <h4 className={'font-sans'}>Aqui encontraras buenas servicios</h4>
                             </div>
@@ -40,7 +40,7 @@ const Feature = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16 text-blue-600">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 9l10.5-3m0 6.553v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 11-.99-3.467l2.31-.66a2.25 2.25 0 001.632-2.163zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 01-1.632 2.163l-1.32.377a1.803 1.803 0 01-.99-3.467l2.31-.66A2.25 2.25 0 009 15.553z" />
                             </svg>
-                            <div>
+                            <div className={'w-full'}>
                                 <h3 className={'font-bold text-xl'}>Variedad Musical</h3>
                                 <h4 className={'font-sans'}>Aqui encontraras buenas servicios</h4>
                             </div>
@@ -49,7 +49,7 @@ const Feature = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-16 h-16 text-blue-600">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
                             </svg>
-                            <div>
+                            <div className={'w-full'}>
                                 <h3 className={'font-bold text-xl'}>Todos los lugares</h3>
                                 <h4 className={'font-sans'}>Aqui encontraras buenas servicios</h4>
                             </div>
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import {useFormik} from "formik";
 
 const Form = () => {
     const cardVariants = {
@@ -17,6 +18,22 @@ const Form = () => {
             }
         }
     };
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            phone: '',
+            location: '',
+            date: '',
+            typeOfShows: '',
+            time: '',
+            message: ''
+        },
+        onSubmit: async (values) => {
+            console.log(values);
+            alert("Mensaje enviado con éxito");
+        }
+    })
 
     return (
         <>
@@ -25,48 +42,48 @@ const Form = () => {
                     <motion.h2 initial="offscreen" whileInView="onscreen" viewport={{ once: true, amount: 0.1}} variants={cardVariants} className={'font-bold md:text-8xl text-5xl '}>Realiza una cotización</motion.h2>
                     <motion.p initial="offscreen" whileInView="onscreen" viewport={{ once: true, amount: 0.1}} variants={cardVariants} className={'md:pr-36 md:pt-10 pr-0 pt-2 font-sans'}>Para una mejor comunicación, completa el formulario y así poder concretar algo bonito. ¡No dudes en hacerlo!</motion.p>
                 </div>
-                <form className={'flex flex-col items-center p-5 shadow rounded-3xl sm:p-10 bg-blue-50 md:w-[700px] w-[300px] md:mt-0 mt-5'}>
+                <form onSubmit={formik.handleSubmit} className={'flex flex-col items-center p-5 shadow rounded-3xl sm:p-10 bg-blue-50 md:w-[700px] w-[300px] md:mt-0 mt-5'}>
                     <div className={'w-full'}>
                         <label htmlFor="name" className="block text-xl font-medium text-gray-700">Nombre</label>
-                        <input type="text" name="name" id="name" autoComplete="given-name" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                        <input value={formik.values.name} onChange={formik.handleChange} {...formik.getFieldProps('name')} type="text" name="name" id="name" autoComplete="given-name" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                     </div>
                     <div className="mt-6 w-full">
                         <label htmlFor="email" className="block text-xl font-medium text-gray-700">Correo</label>
-                        <input type="email" name="email" id="email" autoComplete="email" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                        <input value={formik.values.email} onChange={formik.handleChange} {...formik.getFieldProps('email')} type="email" name="email" id="email" autoComplete="email" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                     </div>
                     <div className="mt-6 w-full">
                         <label htmlFor="phone" className="block text-xl font-medium text-gray-700">Telefono</label>
-                        <input type="text" name="phone" id="phone" autoComplete="phone" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                        <input value={formik.values.phone} onChange={formik.handleChange} {...formik.getFieldProps('phone')} type="text" name="phone" id="phone" autoComplete="phone" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                     </div>
                     <div className={'mt-6 w-full'}>
                         <label htmlFor="location" className="block text-xl font-medium text-gray-700">Dirección</label>
-                        <input type="text" name="location" id="location" autoComplete="location" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                        <input value={formik.values.location} onChange={formik.handleChange} {...formik.getFieldProps('location')} type="text" name="location" id="location" autoComplete="location" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                     </div>
                     <div className={'md:mt-6 mt-3  flex md:flex-row flex-col justify-between w-full '}>
                         <div className={''}>
                             <label htmlFor="date" className="block text-xl font-medium text-gray-700">Fecha</label>
-                            <input type="date" name="date" id="date" autoComplete="date" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                            <input value={formik.values.date} onChange={formik.handleChange} {...formik.getFieldProps('date')} type="date" name="date" id="date" autoComplete="date" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                         </div>
                         <div className={'md:mt-0 mt-6'}>
                             <label htmlFor="typeOfShows" className="block text-xl font-medium text-gray-700">Tipo de Evento</label>
-                            <select id="typeOfShows" name="typeOfShows" autoComplete="typeOfShows" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-                                <option value="1">Cumpleaños</option>
-                                <option value="2">Matrimonio</option>
-                                <option value="3">Misa</option>
-                                <option value="4">Sorpresas</option>
-                                <option value="5">Serenata</option>
-                                <option value="6">Otros</option>
+                            <select  value={formik.values.typeOfShows} onChange={formik.handleChange} onBlur={formik.handleBlur} {...formik.getFieldProps('typeOfShows')} id="typeOfShows" name="typeOfShows" autoComplete="typeOfShows" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                <option value="Cumpleaños">Cumpleaños</option>
+                                <option value="Matrimonio">Matrimonio</option>
+                                <option value="Misa">Misa</option>
+                                <option value="Sorpresas">Sorpresas</option>
+                                <option value="Serenata">Serenata</option>
+                                <option value="Otros">Otros</option>
                             </select>
 
                         </div>
                         <div className={'md:mt-0 mt-6'}>
                             <label htmlFor="time" className="block text-xl font-medium text-gray-700">Hora</label>
-                            <input type="time" name="time" id="time" autoComplete="time" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                            <input value={formik.values.time} onChange={formik.handleChange} {...formik.getFieldProps('time')} type="time" name="time" id="time" autoComplete="time" className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                         </div>
                     </div>
                     <div className="mt-6 w-full">
                         <label htmlFor="message" className="block text-xl font-medium text-gray-700">Mensaje</label>
-                        <textarea id="message" name="message" rows={4} className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" defaultValue={''} />
+                        <textarea value={formik.values.message} onChange={formik.handleChange} {...formik.getFieldProps('message')} id="message" name="message" rows={4} className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" defaultValue={''} />
                     </div>
                     <div className="mt-6">
                         <button type="submit" className="flex items-center justify-between py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
